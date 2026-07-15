fe6f24e3cd15ab136d9e885bac7212b6b7fdaaa7
anishkakhati <anishkak5611@gmail.com>
2026-07-16 00:43:14 +0530

    Move database credentials to .env

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,16 @@
-# config/settings.py
+import os
+from dotenv import load_dotenv
 
+# Load variables from the .env file
+load_dotenv()
+
+# Database Configuration
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Project Information
 PROJECT_NAME = "Finoviq AI"
-
 VERSION = "1.0.0"
-
-DB_HOST = "localhost"
-
-DB_PORT = "5432"
-
-DB_NAME = "Finoviq"
-
-DB_USER = "postgres"
-
-DB_PASSWORD = "Aditya12@"
